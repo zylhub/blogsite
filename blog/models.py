bfb0ff76cf6fa9bec626204d707e4b5bf74d8218
zylhub <zylhub@qq.com>
2021-08-27 22:52:24 +0800

    添加页面列表和详情页

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,15 @@
-from django.db import models
-from django.utils import timezone
 from django.contrib.auth.models import User
+from django.db import models
+from django.urls import reverse
 # Create your models here.
 from slugify import slugify_url
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -10,11 +17,13 @@ class Post(models.Model):
     )
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique_for_date='publish')
-    author = models.ForeignKey(User,on_delete=models.CASCADE,related_name='blog_posts')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES,default='draft')
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    published = PublishedManager()
 
     class Meta:
         ordering = ('-publish',)
@@ -25,3 +34,10 @@ class Post(models.Model):
     def save(self, *args, **kwargs):
         self.slug = slugify_url(self.title)
         super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
